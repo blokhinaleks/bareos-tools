598a9001da9025b156e2c8ec1f770f05b59b5090
ab <ab@noetikos.ru>
2021-08-05 17:14:57 +0300

    add note

--- job_size.sql
+++ job_size.sql
@@ -1,8 +1,9 @@
+--Сколько занимает каждое задание
 select
 	j."name",
 	round (sum(j.jobbytes*1.0) / (1024.0^3),2) size_in_gb 
 from job j
 where j.purgedfiles=0 
---and j.poolid>0 
+and j.poolid>0 
 group by j."name" 
 order by j."name" ;
